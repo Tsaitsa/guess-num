29fe8eaa01c206b49d4ff282a460532c80ac9f8a
Tsai Tsai <am15ocr@gmail.com>
2021-08-22 19:01:30 +0800

    final verson

--- guess.py
+++ guess.py
@@ -1,6 +1,16 @@
+# 產生一個隨機整數1~100(不要印出來)
+# 讓使用者重複輸入數字去猜
+# 猜對的話 印出 "你猜中了！"
+# 猜錯的話 要告訴他 比答案大/小
+
 import random
 
-r = random.randint(1,100) # 1、100都有包括
+start = input('請決定隨機數字範圍開始值： ')
+end = input('請決定隨機數字範圍結束值： ')
+start = int(start)
+end = int(end)
+
+r = random.randint(start,end)
 count = 0 
 while True:
 	count += 1 # count =count + 1
